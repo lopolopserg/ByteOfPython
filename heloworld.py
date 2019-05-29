d80aae74eeec936ff62351d816b41ee079a29202
lopolopserg <lopolopserg@mail.ru>
2019-05-29 13:25:34 +0300

    кодим

--- heloworld.py
+++ heloworld.py
@@ -1,2 +1,2 @@
 #!/usr/bin/env python
-print('Privet Mir')
+print('Privet Mir') # коментарий
---
+++ str_forma.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+name = input('пиши имя: ', )
+vozrast = int(input('пиши свой возраст: ', ))
+print('{} тебе {} лет.'.format(name, vozrast))
